475bdd03c8eb018e3448736bda7a2d00598bcbd9
jose <jose.horas@gmail.com>
2019-02-03 17:59:12 +0100

    Add several changes to improve GUI

--- typing_trainer.py
+++ typing_trainer.py
@@ -1,14 +1,9 @@
-#!/usr/bin/python3
-# write tkinter as Tkinter to be Python 2.x compatible
 import tkinter as tk
-import sys
 
-def hello(event):
-    print("Single Click, Button-l") 
+
 def quit(event):                           
-    print("Double Click, so let's stop") 
     root.destroy()
-def a(event):
+def typing(event):
     global txt
     key = event.keysym
     txt = txt + key
@@ -16,27 +11,35 @@ def a(event):
     my_canvas.itemconfigure(texto, text=txt)
     
 root = tk.Tk()
-my_frame = tk.Frame(root,width=300,height=300)
-my_frame.pack(expand=True, fill=tk.BOTH)
+root.minsize(100, 100)
 
-my_canvas = tk.Canvas(my_frame, width=300, height=300, scrollregion=(0,0,300,500), bg="white")
-vbar= tk.Scrollbar(my_frame,orient=tk.VERTICAL)
+top_frame = tk.Frame(root,width=300,height=300)
+top_frame.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+bottom_frame = tk.Frame(root,width=300,height=300)
+bottom_frame.pack(side=tk.BOTTOM, expand=False, fill=tk.BOTH)
+
+bt = tk.Button(bottom_frame, text='Quit', height=3, width=3)
+bt.pack(side = tk.BOTTOM)
+bt.pack(side = tk.RIGHT, padx=10, pady=5)
+
+my_canvas = tk.Canvas(top_frame, width=300, height=300, scrollregion=(0,0,300,500), bg="white")
+vbar= tk.Scrollbar(top_frame,orient=tk.VERTICAL)
 vbar.pack(side=tk.RIGHT,fill=tk.Y)
 vbar.config(command=my_canvas.yview)
 txt = "hola"
 
-
 my_canvas.config(yscrollcommand=vbar.set, yscrollincrement=10)
 my_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
-my_canvas.create_text(30, 70, text=txt, anchor='sw', tag="texto")
+my_canvas.pack(side=tk.TOP)
+my_canvas.create_text(30, 70, text=txt, anchor='sw', width=200, font=('helvetica', 18), tag="texto")
+
+# text_widget = tk.Text(bottom_frame, wrap=tk.NONE, undo=True, yscrollcommand=vbar.set)
+# text_widget.pack(expand=True, fill="both")
 
 
-bt = tk.Button(root, text='Quit')
-bt.pack(expand=False, side = tk.BOTTOM)
-bt.pack(side = tk.RIGHT, padx=10, pady=5)
 
 bt.bind('<Button-1>', quit)
-root.bind('<Key>', a)
+root.bind('<Key>', typing)
 
 
 root.mainloop()
