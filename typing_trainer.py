65c5e60ba1e39d33c68f7c6ad518de1783ea5d57
jose <jose.horas@gmail.com>
2019-02-03 18:13:42 +0100

    Get rid of global variable txt

--- typing_trainer.py
+++ typing_trainer.py
@@ -4,11 +4,8 @@ import tkinter as tk
 def quit(event):                           
     root.destroy()
 def typing(event):
-    global txt
     key = event.keysym
-    txt = txt + key
-    texto = my_canvas.find_withtag("texto")
-    my_canvas.itemconfigure(texto, text=txt)
+    my_canvas.itemconfigure(1, text=my_canvas.itemcget(texto, "text") + key)
     
 root = tk.Tk()
 root.minsize(70, 38)
@@ -18,7 +15,6 @@ bottom_frame.pack(side=tk.BOTTOM, expand=False, fill=tk.BOTH)
 top_frame = tk.Frame(root,width=300,height=300)
 top_frame.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
-
 bt = tk.Button(bottom_frame, text='Quit')
 bt.pack(side=tk.BOTTOM)
 bt.pack(side=tk.RIGHT, padx=10, pady=5)
@@ -27,20 +23,13 @@ my_canvas = tk.Canvas(top_frame, width=300, height=300, scrollregion=(0,0,300,50
 vbar = tk.Scrollbar(top_frame,orient=tk.VERTICAL)
 vbar.pack(side=tk.RIGHT,fill=tk.Y)
 vbar.config(command=my_canvas.yview)
-txt = "hola"
 
 my_canvas.config(yscrollcommand=vbar.set, yscrollincrement=10)
 my_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
 my_canvas.pack(side=tk.TOP)
-my_canvas.create_text(30, 70, text=txt, anchor='sw', width=200, font=('helvetica', 18), tag="texto")
-
-# text_widget = tk.Text(bottom_frame, wrap=tk.NONE, undo=True, yscrollcommand=vbar.set)
-# text_widget.pack(expand=True, fill="both")
-
-
+my_canvas.create_text(30, 70, text="hola", anchor='sw', width=200, font=('helvetica', 18), tag="texto")
 
 bt.bind('<Button-1>', quit)
 root.bind('<Key>', typing)
-
 
 root.mainloop()
